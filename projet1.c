64349e2d012e01ec40703282897c005505e302b1
Vanir <vanir7314@gmail.com>
2021-10-10 17:24:07 +0200

    Communication of processes done

--- projet1.c
+++ projet1.c
@@ -4,6 +4,20 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+int **createTable(int nbLin){
+	int **table = (int **)malloc(sizeof(int*)*nbLin);
+	int *table2 = (int *)malloc(sizeof(int)*2*nbLin);
+	for(int i = 0 ; i < nbLin ; i++){
+		table[i] = &table2[i*2];
+	}
+	return table;
+}
+ 
+void freeTable(int **table){
+	free(table[0]);
+	free(table);
+}
+
 int main(int argc, char const *argv[])
 {
     if (argc != 2)
@@ -15,7 +29,17 @@ int main(int argc, char const *argv[])
     int n = atoi(argv[1]);
     printf("father's pid -> %d\n",getpid());
     int tubeM[2];   // main pipe
-    int tubes[n][2];    // other pipes
+    int **tubes = createTable(n);   // other pipes
+    //int tubes[n][2];
+    pipe(tubeM);
+    for (int i = 0; i < n; i++)
+    {
+        pipe(tubes[i]);
+    }
+    int x = 4;  // entry value
+    int y;  // return value
+    
+    write(tubes[n-1][1], &x, sizeof(int));
     while (node < n)
     {
         switch (fork())
@@ -25,7 +49,7 @@ int main(int argc, char const *argv[])
             exit(-1);
 
         case 0:
-            printf("%d\n",getpid());
+            close(tubeM[0]);
             for (int i = 0; i < n; i++)
             {
                 if(i == node) {
@@ -37,24 +61,34 @@ int main(int argc, char const *argv[])
                     close(tubes[i][1]);
                 }
             }
-            close(0);
+            // close(0);
             if (node == 0)
             {
-                dup(tubes[n-1][0]);
+                // dup(tubes[n-1][0]);
+                read(tubes[n-1][0], &y, sizeof(int));
                 close(tubes[n-1][0]);
             } else {
-                dup(tubes[node-1][0]);
+                // dup(tubes[node-1][0]);
+                read(tubes[node-1][0], &y, sizeof(int));
                 close(tubes[node-1][0]);
             }
-            close(1);
-            dup(tubes[node][1]);
+            if (node == n-1)
+            {
+                y = y+1;
+            } 
+            write(tubeM[1], &y, sizeof(int));
+            close(tubeM[1]);
+            // close(1);
+            // dup(tubes[node][1]);
+            write(tubes[node][1], &y, sizeof(int));
             close(tubes[node][1]);
             printf("son's pid n°%d -> %d, pid of his father -> %d\n",node,getpid(),getppid());
             exit(0);
 
         default:
             wait(NULL);
-            printf("default's pid (father) -> %d\n",getpid());
+            read(tubeM[0], &y, sizeof(int));
+            printf("default's pid (father) -> %d, main pipe number : %d\n",getpid(),y);
         }
         node++;
     }
@@ -63,5 +97,8 @@ int main(int argc, char const *argv[])
         close(tubes[i][0]);
         close(tubes[i][1]);
     }
+    close(tubeM[1]);
+    close(tubeM[0]);
+    freeTable(tubes);
     return 0;
 }
